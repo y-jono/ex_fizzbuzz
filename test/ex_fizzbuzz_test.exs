508ec31ffc2f3ef72cc7cdfa8db4def34dba99fa
y_jono <pek9dkl@gmail.com>
2016-02-04 23:33:38 +0900

    easy to read \'assert\'

--- test/ex_fizzbuzz_test.exs
+++ test/ex_fizzbuzz_test.exs
@@ -3,32 +3,36 @@ defmodule ExFizzbuzzTest do
   doctest ExFizzbuzz
 
   test "return fizz when input 3 multi number without 15 multi number" do
-    assert 1..100
+    1..100
     |> Stream.filter(&((rem(&1, 3) == 0) && !(rem(&1, 5) == 0)))
     |> Stream.map(&ExFizzbuzz.fizzbuzz/1)
     |> Enum.all?(&(&1 == "fizz"))
+    |> assert
 
   end
 
   test "return fizz when input 5 multi number without 15 multi number" do
-    assert 1..100
+    1..100
     |> Stream.filter(&((rem(&1, 5) == 0) && !(rem(&1, 3) == 0)))
     |> Stream.map(&ExFizzbuzz.fizzbuzz/1)
     |> Enum.all?(&(&1 == "buzz"))
+    |> assert
   end
 
   test "return fizzbuzz when input 15 multi number" do
-    assert 1..100
+    1..100
     |> Stream.filter(&(rem(&1, 15) == 0))
     |> Stream.map(&ExFizzbuzz.fizzbuzz/1)
     |> Enum.all?(&(&1 == "fizzbuzz"))
+    |> assert
   end
 
   test "return numstring when input number without 3 or 5 multi number" do
-    assert 1..100
+    1..100
     |> Stream.filter(&(!rem(&1, 3) == 0 && !rem(&1, 5) == 0))
     |> Stream.map(&ExFizzbuzz.fizzbuzz/1)
     |> Enum.all?(&(&1 == to_string(&1)))
+    |> assert
   end
 
 end
